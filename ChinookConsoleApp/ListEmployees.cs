679337411e86f042205cc4e6d8d03e28babc2b30
Nathan Gonzalez <nathan@copypastedeveloper.com>
2017-08-15 19:19:16 -0500

    listing chinook employees

---
+++ ChinookConsoleApp/ListEmployees.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ChinookConsoleApp
+{
+    public class ListEmployees
+    {
+        public void List()
+        {
+            using (var connection = new SqlConnection("Server=(local);Database=chinook;Trusted_Connection=True;"))
+            {
+                var employeeListCommand = connection.CreateCommand();
+
+                employeeListCommand.CommandText = "select employeeid as Id, firstname + ' ' + lastname as fullname from Employee";
+
+                try
+                {
+                    connection.Open();
+                    var reader = employeeListCommand.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["Id"]}.) {reader["FullName"]}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }
+        }
+    }
+}
